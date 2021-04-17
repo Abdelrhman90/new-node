5d12eb44100c20f00fa51e8cdb3105616c1859a4
unknown <abdelrhmanmkting@gmail.com>
2021-04-17 10:59:34 +0200

    Travis

--- config/ci.js
+++ config/ci.js
@@ -3,6 +3,6 @@ module.exports = {
     "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
   googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
   mongoURI: "mongodb://127.0.0.1:27017/blog-ci",
-  redisUrl: "redis://127.0.0.1:6379",
   cookieKey: "123123123",
+  redisUrl: "redis://127.0.0.1:6379",
 };
